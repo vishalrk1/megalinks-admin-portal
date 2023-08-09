1ddc475fb1d557eb03048f494b77488e3323f7e2
Vishal.Karangale <Vishal.Karangale@ril.com>
2023-08-09 18:15:46 +0530

    nav-bar fixes

--- app/(dashboard)/[userId]/layout.tsx
+++ app/(dashboard)/[userId]/layout.tsx
@@ -23,9 +23,7 @@ export default async function DashboardLayout({
 
     return (
         <>
-            <div>
-                <Navbar />
-            </div>
+            <Navbar />
             {children}
         </>
     );

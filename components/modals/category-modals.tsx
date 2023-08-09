3090a64eb551f2f9ce0ee5c6b5ea8200619f96f0
Vishal.Karangale <Vishal.Karangale@ril.com>
2023-08-09 18:21:08 +0530

    adding toast in category Modal

--- components/modals/category-modals.tsx
+++ components/modals/category-modals.tsx
@@ -18,6 +18,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from 'react-hot-toast';
 
 const formSchema = z.object({
   name: z.string().min(1),

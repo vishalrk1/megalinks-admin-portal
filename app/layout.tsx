a2b4bac5cc45475fe29f5cdfadd3a3d8470a10d1
Vishal.Karangale <Vishal.Karangale@ril.com>
2023-09-05 18:16:30 +0530

    font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/provider/modal-provider";
 import { ToasterProvider } from '@/provider/toast-provider';
 import { ThemeProvider } from "@/provider/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Poppins({weight: "400"}); // Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Megalinks admin Dashboard",
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={font.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <ToasterProvider />
             <ModalProvider />
